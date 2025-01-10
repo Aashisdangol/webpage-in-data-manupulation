0caf766cebff659f8fb93927a382678a7dcccef2
Aashis <12200199@students.koi.edu.au>
2025-01-10 14:08:10 +1100

    finidhed the html part

---
+++ script.js
@@ -0,0 +1,47 @@
+const topic1 = document.querySelector(".topic")
+topic1.innerText = ("Data Manupulation")
+
+const discription1 = document.querySelector(".discription ")
+discription1.innerText = ("The process of adjusting, organizing, or restructuring data to make it more useful, understandable, or in a desired format. It can involve various operations such as sorting, filtering, transforming, and aggregating data to derive insights or modify it for a specific use case.")
+
+const type = document.querySelector(".type-1 ")
+type.innerText = ("Types of Data Manupulation")
+
+// Table 
+let dataType = document.querySelector("#Datatype")
+dataType.textContent = "Primitive Data Types"
+let Description = document.querySelector("#Description")
+Description.textContent = "Description"
+
+
+// Table body 
+
+let dataType1 = document.querySelector("#data-type-1-1")
+dataType1.textContent = "Number"
+let description1 = document.querySelector("#description-1-1")
+description1.textContent = "	Represents integers and floating-point numbers."
+
+
+let dataType2 = document.querySelector("#data-type-1-2")
+dataType2.textContent = "String"
+let description2 = document.querySelector("#description-1-2")
+description2.textContent = "Sequence of characters used to represent text."
+
+
+
+let dataType3 = document.querySelector("#data-type-1-3")
+dataType3.textContent = "Boolean"
+let description3 = document.querySelector("#description-1-3")
+description3.textContent = "Logical data type with two values: true or false."
+
+
+let dataType4 = document.querySelector("#data-type-1-4")
+dataType4.textContent = "Array"
+let description4 = document.querySelector("#description-1-4")
+description4.textContent = "Represents an ordered collection of values."
+
+
+let dataType6 = document.querySelector("#data-type-1-6")
+dataType6.textContent = "Object"
+let description6 = document.querySelector("#description-1-6")
+description6.textContent = " Represents key-value pairs or properties."
